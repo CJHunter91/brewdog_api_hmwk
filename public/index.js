7c3ca8098b34483c24609c6d22572256d69765f3
Chris Hunter <orkem2@gmail.com>
2017-09-06 15:54:57 +0100

    added make request and request complete

--- public/index.js
+++ public/index.js
@@ -1,4 +1,36 @@
 var app = function(){
+  var beers = localStorage.getItem('a')
+  makeRequest(requestComplete);
+  listBeers();
+
+}
+
+var getBeers = function(){
+
+}
+
+var listBeers = function(){
+  var beers = getBeers();
+}
+
+var requestComplete = function(){
+  console.log("requestComplete");
+  if(this.status !== 200) return;
+  var jsonString = this.responseText;
+  localStorage.setItem('all_beers', jsonString);
+  var beers = JSON.parse(jsonString);
+}
+
+var makeRequest = function(callback){
+  var url = "https://api.punkapi.com/v2/beers"
+  // create request object
+  var request = new XMLHttpRequest();
+  //add the get request
+  request.open('GET', url);
+  //add a listener to execute when the data has loaded
+  request.addEventListener('load', callback);
+  //send the request
+  request.send()
 
 }
 
